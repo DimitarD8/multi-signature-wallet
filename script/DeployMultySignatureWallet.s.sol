a8bfb0b97bf3c02941893abf43f1c3a0896c8135
DimitarD8 <ddimitrov01@abv.bg>
2024-03-09 18:12:22 +0200

    Create and deploy MultySig wallet

---
+++ script/DeployMultySignatureWallet.s.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+
+import {Script} from "forge-std/Script.sol";
+import {MultiSignatureWallet} from "../src/MultiSignatureWallet.sol";
+
+contract DeployMultySignatureWallet is Script {
+    function run() external returns (MultiSignatureWallet) {
+        vm.startBroadcast();
+
+        address owner = msg.sender;
+        address[] memory initialOwners = new address[](3);
+        initialOwners[0] = owner;
+        MultiSignatureWallet wallet = new MultiSignatureWallet(
+            initialOwners,
+            1
+        );
+
+        vm.stopBroadcast();
+        return wallet;
+    }
+}
